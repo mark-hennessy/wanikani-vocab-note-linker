77a7a99e7726e3d65aa97ff0522d30769f685e31
Mark Hennessy <markhennessy@deinestudienfinanzierung.de>
2019-11-23 11:02:48 +0100

    More description updates

--- src/index.js
+++ src/index.js
@@ -51,42 +51,46 @@ document.getElementById('app').innerHTML = `
 // @match        https://www.wanikani.com/vocabulary/*
 // ==/UserScript==
 
-// Creates links for vocabulary in the "Meaning Note" and "Reading Note" sections.
+/*
+Creates links for vocabulary in the "Meaning Note" and "Reading Note" sections.
 
-// Example Meaning Note
-// ==
-// 木材（もくざい）Wood, Lumber 木材
-// 材木（ざいもく）Lumber, Timber, Wood
-//
-// Some text
+Example Meaning Note
+==
+木材（もくざい）Wood, Lumber 木材
+材木（ざいもく）Lumber, Timber, Wood
+Some text
 
-// Constraints & Limitations
-// ==
-// The script only works for vocabulary at the start of each new line
-// The script only works for vocabulary immediately followed by a Japanese opening parenthesis（
-// Chrome for mobile does not allow add-ons and thus does not support Tampermonkey UserScripts
-// The "All" link will only work if you enable multiple popups/tabs in your browser settings
+Constraints & Limitations
+==
+The script only works for vocabulary at the start of each new line
+The script only works for vocabulary immediately followed by a Japanese opening parenthesis（
+Chrome for mobile does not allow add-ons and thus does not support Tampermonkey UserScripts
+The "All" link will only work if you enable multiple popups/tabs in your browser settings
 
-// How to use UserScripts on Firefox for mobile
-// ==
-// Install Firefox for mobile
-// Install the Tampermonkey extension
-// Visit GreasyFork.org to install this script
+How to use UserScripts on Firefox for mobile
+==
+Install Firefox for mobile
+Install the Tampermonkey extension
+Visit GreasyFork.org to install this script
 
-// Enable multiple popups/tabs in Chrome
-// =
-// Click the "All" link
-// Check the URL bar for a notification icon telling you that popups were blocked
-// Click the icon and tell chrome to stop blocking popups from WaniKani
+Enable multiple popups/tabs in Chrome
+==
+Click the "All" link
+Check the URL bar for a notification icon telling you that popups were blocked
+Click the icon and tell chrome to stop blocking popups from WaniKani
 
-// Enable multiple popups/tabs in Firefox for Android (probably iOS as well)
-// =
-// Type "about:config" in the URL bar
-// Search for "popups"
-// Click "dom.block_multiple_popups" to change the value to "false"
+Enable multiple popups/tabs in Firefox for Android (probably iOS as well)
+==
+Type "about:config" in the URL bar
+Search for "popups"
+Click "dom.block_multiple_popups" to change the value to "false"
 
-// GreasyFork: https://greasyfork.org/en/scripts/392752-wanikani-vocabulary-linker
-// GitHub: https://github.com/mark-hennessy/wanikani-vocabulary-linker
+Useful Links
+==
+CodeSandbox: https://codesandbox.io/s/wanikani-vocabulary-linker-jzejl
+GreasyFork: https://greasyfork.org/en/scripts/392752-wanikani-vocabulary-linker
+GitHub: https://github.com/mark-hennessy/wanikani-vocabulary-linker
+*/
 
 const currentURL = decodeURI(window.location.href);
 const currentVocab = currentURL.split('/').pop();
