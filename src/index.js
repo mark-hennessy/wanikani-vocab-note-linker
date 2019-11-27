f1229ffc4732dfbb4611f664fd8364ef7a2a84d1
Mark Hennessy <markhennessy@deinestudienfinanzierung.de>
2019-11-27 06:40:24 +0100

    Bumped version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // @name         WaniKani Vocab Note Linker
 // @namespace    http://tampermonkey.net/
 // @description  Creates links for vocabulary in the Meaning Note and Reading Note sections.
-// @version      1.4.3
+// @version      1.4.4
 // @author       Mark Hennessy
 // @match        https://www.wanikani.com/vocabulary/*
 // @match        https://www.wanikani.com/kanji/*
